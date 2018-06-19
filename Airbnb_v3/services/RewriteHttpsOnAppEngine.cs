97cd993969c7a2813aebfa586f7ae9b9d755261c
Bart van de Bovenkamp <bvdbovenkamp@tricode.nl>
2018-06-19 03:58:42 +0200

    added class to redirect all http calls to https

---
+++ Airbnb_v3/services/RewriteHttpsOnAppEngine.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.AspNetCore.Rewrite;
+
+namespace InsideAirBNB.App.Services
+{
+    public enum HttpsPolicy { Required, Optional };
+
+    public class RewriteHttpsOnAppEngine : IRule
+    {
+        readonly HttpsPolicy _httpsPolicy;
+        public RewriteHttpsOnAppEngine(HttpsPolicy httpsPolicy)
+        {
+            _httpsPolicy = httpsPolicy;
+        }
+
+        static PathString s_healthCheckPathString = new PathString("/_ah/health");
+
+        public static bool Rewrite(HttpRequest request)
+        {
+            if (request.Scheme == "https")
+            {
+                return true;  // Already https.
+            }
+
+            string proto = request.Headers["X-Forwarded-Proto"].FirstOrDefault();
+
+            if (proto == "https")
+            {
+                request.IsHttps = true;
+                request.Scheme = "https";
+                return true;
+            }
+
+            if (request.Path.StartsWithSegments(s_healthCheckPathString))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        void IRule.ApplyRule(RewriteContext context)
+        {
+            var request = context.HttpContext.Request;
+            bool wasSecure = Rewrite(request);
+
+            if (!wasSecure && _httpsPolicy == HttpsPolicy.Required)
+            {
+                var newUrl = string.Concat(
+                        "https://",
+                        request.Host.ToUriComponent(),
+                        request.PathBase.ToUriComponent(),
+                        request.Path.ToUriComponent(),
+                        request.QueryString.ToUriComponent());
+
+                var action = new RedirectResult(newUrl);
+
+                ActionContext actionContext = new ActionContext()
+                {
+                    HttpContext = context.HttpContext
+                };
+
+                action.ExecuteResult(actionContext);
+                context.Result = RuleResult.EndResponse;
+            }
+        }
+
+    }
+}
